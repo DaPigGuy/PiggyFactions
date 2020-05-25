c3c20fdf02c3ca29118ca7c9242d80d718e3d5ed
DaPigGuy <mcpepig123@gmail.com>
2020-05-25 14:58:20 -0700

    Fixed crash if Poggit can't be reached by update check

--- src/DaPigGuy/PiggyFactions/tasks/CheckUpdatesTask.php
+++ src/DaPigGuy/PiggyFactions/tasks/CheckUpdatesTask.php
@@ -14,7 +14,7 @@ class CheckUpdatesTask extends AsyncTask
 {
     public function onRun(): void
     {
-        $this->setResult(Internet::getURL("https://poggit.pmmp.io/releases.json?name=PiggyFactions"));
+        $this->setResult([Internet::getURL("https://poggit.pmmp.io/releases.json?name=PiggyFactions", 10, [], $error), $error]);
     }
 
     public function onCompletion(Server $server): void
@@ -22,7 +22,12 @@ class CheckUpdatesTask extends AsyncTask
         $plugin = PiggyFactions::getInstance();
         try {
             if ($plugin->isEnabled()) {
-                $data = json_decode($this->getResult(), true);
+                $results = $this->getResult();
+
+                $error = $results[1];
+                if ($error !== null) throw new Exception($error);
+
+                $data = json_decode($results[0], true);
                 if (version_compare($plugin->getDescription()->getVersion(), $data[0]["version"]) === -1) {
                     if ($server->getPluginManager()->isCompatibleApi($data[0]["api"][0]["from"])) {
                         PiggyFactions::getInstance()->getLogger()->info("PiggyFactions v" . $data[0]["version"] . " is available for download at " . $data[0]["artifact_url"] . "/PiggyFactions.phar");
