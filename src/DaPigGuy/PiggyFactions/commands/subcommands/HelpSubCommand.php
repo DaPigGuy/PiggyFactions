9c29205b08d7591906d7b2953225cf7f6443373d
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2023-06-19 04:58:08 +0200

    /f help crash with negative int  (#133)
    * Fix negative large input
    
    * Avoid float input (don't really know if it's really useful)
    
    * Be accurate with PHP Stan

--- src/DaPigGuy/PiggyFactions/commands/subcommands/HelpSubCommand.php
+++ src/DaPigGuy/PiggyFactions/commands/subcommands/HelpSubCommand.php
@@ -33,8 +33,9 @@ class HelpSubCommand extends FactionSubCommand
 
         $commandsPerPage = $sender instanceof Player ? self::COMMANDS_PER_PAGE : count($subcommands);
         $maxPages = (int)ceil(count($subcommands) / $commandsPerPage);
-        $page = $args["page"] ?? 1;
+        $page = (int)($args["page"] ?? 1);
         $page = min($page, $maxPages);
+        if ($page < 1) $page = 1;
         $pageCommands = array_slice($subcommands, $commandsPerPage * ($page - 1), $commandsPerPage);
 
         $language = $sender instanceof Player ? $this->plugin->getLanguageManager()->getPlayerLanguage($sender) : $this->plugin->getLanguageManager()->getDefaultLanguage();
